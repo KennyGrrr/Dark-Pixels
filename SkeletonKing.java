5f8ae498cc247c06a502338c5cc0e3086b76352c
TealBazooka <coryslaught@gmail.com>
2015-01-29 16:07:48 -0800

    Ready for thing Skeleton king image added
    needs to have skeleton king kill character.

---
+++ SkeletonKing.java
@@ -0,0 +1,59 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class SkeletonKing here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SkeletonKing extends Actor
+{
+    /**
+     * Act - do whatever the SkeletonKing wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+public void act() 
+    {
+        // Add your action code here.
+        randomMovement();
+        death();
+        //kill will give an error when death occurs
+    }    
+    public void randomMovement()
+    {
+        //Poorly executed random movement function
+        int i = 0;
+        if( Greenfoot.getRandomNumber(50) == 0)
+        {
+            //up
+            setLocation(getX(), getY() - 10);
+        }
+        if( Greenfoot.getRandomNumber(50) == 1)
+        {
+            //right
+            setLocation(getX() + 20, getY());
+        }
+        if(Greenfoot.getRandomNumber(50) == 2)
+        {
+            //down
+            setLocation(getX(), getY() + 10);
+        }
+        if(Greenfoot.getRandomNumber(50) == 3)
+        {
+            // left
+            setLocation(getX() - 10, getY());
+        }
+    }
+    public void death()
+    {
+        // Death: if the iron sword touches this actor it is removed.
+        Actor ironsword;
+        ironsword = getOneObjectAtOffset(0, 0, IronSword.class);
+        if (isTouching(IronSword.class) == true)
+        {
+            World world;
+            world = getWorld();
+            world.removeObject(this);
+        }
+    } 
+}
